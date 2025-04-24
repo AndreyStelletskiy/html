f102b8c4334c07c1743cb9229b8eed6ea34ce4cc
Andrey S <a.stelletskiy@ya.ru>
2025-04-24 17:33:15 +0300

    Уведомление об отправки письма Всплывающее окно

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//Начало программы
+document.addEventListener("DOMContentLoaded",function(){
+
+const send = document.getElementById("send")
+const mes = document.getElementById("mes")
+
+send.addEventListener("click",() =>{
+    console.log(mes.value)
+    
+    if (mes.value.trim()!=""){
+        modalWndow('Ваше сообщение передано владельцу данного сайта', 1200);
+
+        function modalWndow(info, t) {
+                const p = document.querySelector('.modals');
+                p.innerHTML = info;
+                p.classList.add('vizz');
+                let time = setTimeout(() => {
+                    p.classList.remove('vizz');
+                    p.innerHTML = "";
+                }, t);
+        }
+    }
+
+    mes.value=""
+
+})
+
+})
